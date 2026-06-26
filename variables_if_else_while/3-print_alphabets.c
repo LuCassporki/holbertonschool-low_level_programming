2a76a27e9647e17e5a64b0a2a9902129b13f3f45
Alexander <lucas.momot.rmz@gmail.com>
2026-06-26 14:39:38 +0200

    t3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,23 @@ int main(void)
 {
 	char lettre;
 
+	/* 1. Boucle pour les minuscules ('a' à 'z') */
 	lettre = 'a';
 	while (lettre <= 'z')
 	{
 		putchar(lettre);
 		lettre++;
 	}
+
+	/* 2. Boucle pour les majuscules ('A' à 'Z') */
+	lettre = 'A';
+	while (lettre <= 'Z')
+	{
+		putchar(lettre);
+		lettre++;
+	}
+
+	/* 3. Saut de ligne final */
 	putchar('\n');
 
 	return (0);
