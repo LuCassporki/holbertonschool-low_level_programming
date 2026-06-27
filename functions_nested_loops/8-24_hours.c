c39f61b927a5d097f402fe725093ae2bc57ca148
Alexander <lucas.momot.rmz@gmail.com>
2026-06-27 19:53:57 +0200

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,1 +1,34 @@
+#include "main.h"
 
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int heure, minute;
+
+	heure = 0;
+	while (heure < 24)
+	{
+		minute = 0;
+		while (minute < 60)
+		{
+			/* Affichage de l'heure : Dizaine puis Unité */
+			_putchar((heure / 10) + '0');
+			_putchar((heure % 10) + '0');
+
+			_putchar(':');
+
+			/* Affichage de la minute : Dizaine puis Unité */
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+
+			_putchar('\n');
+			minute++;
+		}
+		heure++;
+	}
+}
