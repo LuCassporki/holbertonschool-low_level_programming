902cb96791751a678a4b88fcc218d3a8cb668a4c
Alexander <lucas.momot.rmz@gmail.com>
2026-06-28 15:03:24 +0200

    t22

--- intro_debugging/row_sums.c
+++ intro_debugging/row_sums.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
 
+/**
+ * row_sum - Computes the sum of a single row
+ * @row: The current row number
+ * @cols: Total number of columns
+ *
+ * Return: The sum of the row
+ */
+int row_sum(int row, int cols)
+{
+	int c;
+	int sum = 0;
+
+	for (c = 1; c <= cols; c++)
+	{
+		sum += row * c;
+	}
+
+	return (sum);
+}
+
+/**
+ * total_sum - Computes the grand total sum of all rows
+ * @rows: Total number of rows
+ * @cols: Total number of columns
+ *
+ * Return: The grand total sum
+ */
+int total_sum(int rows, int cols)
+{
+	int r;
+	int total = 0;
+
+	for (r = 1; r <= rows; r++)
+	{
+		total += row_sum(r, cols);
+	}
+
+	return (total);
+}
+
+/* -- DO NOT Modify the code below this line -- */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%d\n", total_sum(3, 3));
+	return (0);
+}
