5235b92c23ba545d3df3a286608b580c3892b902
Alexander <lucas.momot.rmz@gmail.com>
2026-06-28 19:19:43 +0200

    t102

--- more_functions_nested_loops/main_0.c
+++ more_functions_nested_loops/main_0.c
@@ -1,11 +1,40 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - Prints numbers from 1 to 100 with Fizz, Buzz and FizzBuzz rules
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    char c;
+	int i;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
 }
