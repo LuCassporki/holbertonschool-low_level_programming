a2d1954b610ba04a850aff63adaed27c637b7a0d
Alexander <lucas.momot.rmz@gmail.com>
2026-06-28 19:57:10 +0200

    t105

--- more_functions_nested_loops/main_0.c
+++ more_functions_nested_loops/main_0.c
@@ -1,33 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- *print_triangle - Prints a triangle, followed by a new line
- * @size: The size of the triangle
- *
- * Return: void
- */
-void print_triangle(int size)
+int main(void)
 {
-	int row, spaces, hashes;
-
-	if (size > 0)
-	{
-		for (row = 1; row <= size; row++)
-		{
-			for (spaces = 0; spaces < size - row; spaces++)
-			{
-				_putchar(' ');
-			}
-			for (hashes = 0; hashes < row; hashes++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
-	else
-	{
-		_putchar('\n');
-	}
+    print_triangle(10);
+    return (0);
 }
