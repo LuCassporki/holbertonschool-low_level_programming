27920d95d824e349b237a3dc7f970f8ef98588c1
Alexander <lucas.momot.rmz@gmail.com>
2026-06-28 14:56:45 +0200

    t0

--- intro_debugging/multiplication_table.c
+++ intro_debugging/multiplication_table.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+/**
+ * print_table - Prints a multiplication table of a given size
+ * @size: The width and height of the table
+ *
+ * Return: void
+ */
+void print_table(int size)
+{
+	int row, col;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (col = 1; col <= size; col++)
+		{
+			printf("%d ", row * col);
+		}
+		printf("\n");
+	}
+}
+
+/* -- DO NOT Modify the code below this line -- */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_table(4);
+	return (0);
+}
