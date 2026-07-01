d31d0da949c67a65ba5200ebaa84d4f97cac482e
Alexander <lucas.momot.rmz@gmail.com>
2026-07-01 19:53:55 +0200

    t4-2

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,28 +1,26 @@
 #include "main.h"
+
 /**
  * print_rev - Prints a string in reverse, followed by a new line.
- * @s: A pointer to the string to print.
- *
- * Return: void.
+ * @s: A pointer to the string.
  */
 void print_rev(char *s)
 {
-    int len;
+	int len;
 
 	len = 0;
-
-	while (*s != '\0')
+	/* Étape 1 : On calcule la longueur sans déplacer s */
+	while (s[len] != '\0')
 	{
 		len++;
-		s++;
 	}
 
+	/* Étape 2 : On part de la fin et on recule jusqu'à 0 inclus */
+	len--; /* On se place sur le dernier caractère avant '\0' */
 	while (len >= 0)
 	{
-		_putchar(*s);
-		s--; /* On déplace notre vecteur d'un octet vers la case suivante */
-	    len--;
-    }
-
+		_putchar(s[len]);
+		len--;
+	}
 	_putchar('\n');
 }
