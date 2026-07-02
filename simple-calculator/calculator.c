27dc22c1404288dbdb222449edf9317394084c5d
Alexander <lucas.momot.rmz@gmail.com>
2026-07-03 01:30:50 +0200

    t5

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -7,6 +7,8 @@
  */
 void display_menu(void)
 {
+	printf("************************************\n");
+	printf("************************************\n");
 	printf("Simple Calculator\n");
 	printf("1) Add\n");
 	printf("2) Subtract\n");
@@ -63,22 +65,44 @@ int main(void)
             {
                 get_operands(&A, &B);
 				result = A + B;
-				printf("Result: %d\n", result);
+				printf("-+-+-+-+-+-+-+-+-+-+-+-+-\n");
+                printf("Result: %d\n", result);
+                printf("-+-+-+-+-+-+-+-+-+-+-+-+-\n");
             }
             else if (choice == 2)
             {
               get_operands(&A, &B);
 				result = A - B;
-				printf("Result: %d\n", result);
+				printf("-+-+-+-+-+-+-+-+-+-+-+-+-\n");
+                printf("Result: %d\n", result);
+                printf("-+-+-+-+-+-+-+-+-+-+-+-+-\n");
             }
             else if (choice == 3)
             {
                 get_operands(&A, &B);
                 result = A * B;
+                printf("-+-+-+-+-+-+-+-+-+-+-+-+-\n");
                 printf("Result: %d\n", result);
+                printf("-+-+-+-+-+-+-+-+-+-+-+-+-\n");
             }
             else
             {
+                get_operands(&A, &B);
+                if(B == 0)
+                {
+                    printf("---------------------------\n");
+                    printf("Error: division by zero\n");
+                    printf("---------------------------\n");
+                    printf("999999999999999999999\n");
+                    printf("execute order 66!\n");
+                    printf("666666666666666666666\n");
+                }
+                else{
+                    double final_res = (double)A / B;
+                    printf("-+-+-+-+-+-+-+-+-+-+-+-+-\n");
+                    printf("Result: %g\n", final_res);
+                    printf("-+-+-+-+-+-+-+-+-+-+-+-+-\n");
+                }
 
             }
 		}
