80975e31344136f55bf1f3608bf1d1c536c03307
Alexander <lucas.momot.rmz@gmail.com>
2026-07-02 18:34:27 +0200

    t2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The pointer to the string to search.
+ * @c: The character to locate.
+ *
+ * Return: A pointer to the first occurrence of c,
+ * or NULL if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	/* On boucle en incluant le traitement du \0 grâce à la condition post-incrément */
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+
+	/* Sécurité POSIX : Si on cherche le caractère '\0' lui-même */
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+
+	return (NULL);
+}
