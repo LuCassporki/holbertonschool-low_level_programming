e1d71562192bb52a485778c6e6af284f18959ce2
Alexander <lucas.momot.rmz@gmail.com>
2026-07-07 15:08:21 +0200

    t2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,45 +15,14 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len1 = 0;
 	unsigned int len2 = 0;
 	unsigned int i, j;
-
-	/* Étape 1 : Sécurisation et traitement des pointeurs NULL */
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	/* Étape 2 : Mesure du premier segment */
-	while (s1[len1] != '\0')
-		len1++;
-
-	/* Étape 3 : Mesure du second segment */
-	while (s2[len2] != '\0')
-		len2++;
-
-	/* Étape 4 : Allocation de la zone globale unifiée (+1 pour le \0) */
+	if (s1 == NULL) s1 = "";
+	if (s2 == NULL) s2 = "";
+	while (s1[len1] != '\0') len1++;
+	while (s2[len2] != '\0') len2++;
 	concat = malloc((len1 + len2 + 1) * sizeof(char));
-
-	/* Étape 5 : Contrôle de sécurité de l'axe mémoire */
-	if (concat == NULL)
-	{
-		return (NULL);
-	}
-
-	/* Étape 6 : Injection du premier segment s1 */
-	for (i = 0; i < len1; i++)
-	{
-		concat[i] = s1[i];
-	}
-
-	/* Étape 7 : Injection du second segment s2 juste à la suite */
-	for (j = 0; j < len2; j++, i++)
-	{
-		concat[i] = s2[j];
-	}
-
-	/* Étape 8 : Enclenchement du signal de fin de chaîne */
+	if (concat == NULL) {return (NULL);}
+	for (i = 0; i < len1; i++){concat[i] = s1[i];}
+	for (j = 0; j < len2; j++, i++){concat[i] = s2[j];}
 	concat[i] = '\0';
-
-	/* Étape 9 : Libération du pointeur vers l'application */
 	return (concat);
 }
