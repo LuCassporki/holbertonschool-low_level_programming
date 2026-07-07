3eee1914312f990721aee8f1c16f1dca258bd223
Alexander <lucas.momot.rmz@gmail.com>
2026-07-07 15:16:51 +0200

    t2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,6 +15,7 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len1 = 0;
 	unsigned int len2 = 0;
 	unsigned int i, j;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -25,9 +26,7 @@ char *str_concat(char *s1, char *s2)
 		len2++;
 	concat = malloc((len1 + len2 + 1) * sizeof(char));
 	if (concat == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < len1; i++)
 	{
 		concat[i] = s1[i];
